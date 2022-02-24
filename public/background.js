b318656f807857e792e93f1e700c4b08b235fcb8
Leon Oleschko <22222725+leoole100@users.noreply.github.com>
2022-02-24 18:15:29 +0100

    added background scaling

--- public/background.js
+++ public/background.js
@@ -5,6 +5,8 @@ function initializeBackground(){
 // render the background will be replaced with video stream
 function renderBackground(){
 	//clearCanvas(b);
-	//b.fillStyle = "#106050";
+	b.fillStyle = "black";
 	b.fillRect(0, 0, dim[0], dim[1]);
+	b.fillStyle = "#106050";
+	b.fillRect(0,(dim[1] - 9/16 * dim[0])/2 , dim[0], 9/16 * dim[0]);
 }
